10a44e195c6e9d0c06f223cda7335c7cd5fd3da7
gsoffice0617 <gsoffice0617@gmail.com>
2025-11-17 16:33:33 +0900

    1. Bridge dll 생성중

---
+++ GemBridge/BridgeExports.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using SecsGemLib;
+
+public static class BridgeExports
+{
+    private static GemApi? _gem;
+
+    [UnmanagedCallersOnly(EntryPoint = "Gem_Init")]
+    public static int Gem_Init()
+    {
+        //_gem = new GemApi();
+        return 1;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "Gem_Connect")]
+    public static int Gem_Connect(IntPtr ipPtr, int port, bool passive)
+    {
+        string ip = Marshal.PtrToStringAnsi(ipPtr)!;
+
+        var t = _gem!.ConnectAsync(ip, port, passive);
+        t.Wait();
+
+        return t.Result ? 1 : 0;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "Gem_Disconnect")]
+    public static void Gem_Disconnect()
+    {
+        _gem?.Disconnect();
+    }
+}
